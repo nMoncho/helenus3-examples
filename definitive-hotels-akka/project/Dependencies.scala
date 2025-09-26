b32fd5ad39e82e439947bb1929b9a380d09044ff
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2025-09-26 13:52:32 +0200

    Update examples to v1.1.2

--- definitive-hotels-akka/project/Dependencies.scala
+++ definitive-hotels-akka/project/Dependencies.scala
@@ -1,12 +1,12 @@
 import sbt.*
 
 object Dependencies {
-  lazy val helenus       = "net.nmoncho"          %% "helenus-core"     % "1.0.0"
-  lazy val helenusAkka   = "net.nmoncho"          %% "helenus-akka"     % "1.0.0"
-  lazy val ossJavaDriver = "org.apache.cassandra"  % "java-driver-core" % "4.18.1"
+  lazy val helenus       = "net.nmoncho"          %% "helenus-core"     % "1.1.2"
+  lazy val helenusAkka   = "net.nmoncho"          %% "helenus-akka"     % "1.1.2"
+  lazy val ossJavaDriver = "org.apache.cassandra"  % "java-driver-core" % "4.19.0"
   lazy val cassandraUnit = "org.cassandraunit"     % "cassandra-unit"   % "4.3.1.0"
-  lazy val scalaTest     = "org.scalatest"        %% "scalatest"        % "3.2.15"
-  lazy val jna           = "net.java.dev.jna"      % "jna"              % "5.12.1" // Fixes M1 JNA
+  lazy val scalaTest     = "org.scalatest"        %% "scalatest"        % "3.2.19"
+  lazy val jna           = "net.java.dev.jna"      % "jna"              % "5.18.0" // Fixes M1 JNA
 
   lazy val akkaStream  = ("com.typesafe.akka"  %% "akka-stream" % "2.6.20").cross(CrossVersion.for3Use2_13)
   lazy val alpakka     = ("com.lightbend.akka" %% "akka-stream-alpakka-cassandra" % "4.0.0").cross(CrossVersion.for3Use2_13)
--- definitive-hotels-async/project/Dependencies.scala
+++ definitive-hotels-async/project/Dependencies.scala
@@ -1,13 +1,13 @@
 import sbt._
 
 object Dependencies {
-  lazy val helenus       = "net.nmoncho"         %% "helenus-core"     % "1.0.0"
-  lazy val ossJavaDriver = "org.apache.cassandra" % "java-driver-core" % "4.18.1"
+  lazy val helenus       = "net.nmoncho"         %% "helenus-core"     % "1.1.2"
+  lazy val ossJavaDriver = "org.apache.cassandra" % "java-driver-core" % "4.19.0"
   lazy val cassandraUnit = "org.cassandraunit"    % "cassandra-unit"   % "4.3.1.0"
-  lazy val scalaTest     = "org.scalatest"       %% "scalatest"        % "3.2.15"
-  lazy val jna = "net.java.dev.jna" % "jna" % "5.12.1" // Fixes M1 JNA issue
+  lazy val scalaTest     = "org.scalatest"       %% "scalatest"        % "3.2.19"
+  lazy val jna           = "net.java.dev.jna"     % "jna"              % "5.18.0" // Fixes M1 JNA issue
 
-  lazy val pekkoStream    = "org.apache.pekko" %% "pekko-stream"               % "1.0.2"
-  lazy val pekkoConnector = "org.apache.pekko" %% "pekko-connectors-cassandra" % "1.0.1"
-  lazy val pekkoTestKit   = "org.apache.pekko" %% "pekko-testkit"              % "1.0.2"
+  lazy val pekkoStream    = "org.apache.pekko" %% "pekko-stream"               % "1.2.1"
+  lazy val pekkoConnector = "org.apache.pekko" %% "pekko-connectors-cassandra" % "1.2.0"
+  lazy val pekkoTestKit   = "org.apache.pekko" %% "pekko-testkit"              % "1.2.1"
 }
